290735b9dca3e6d4743cc61dd4706246e80af57f
KDawgDK <48488232+KDawgDK@users.noreply.github.com>
2023-10-03 10:06:01 +0200

    grouped windows feature install doesn't work

--- SDE-Stuff/Windows-Powershell-setup-1.ps1
+++ SDE-Stuff/Windows-Powershell-setup-1.ps1
@@ -5,7 +5,7 @@ $adapter = (Get-NetAdapter -Physical | Select-Object -First 1).ifIndex
 New-NetIPAddress -IPAddress 192.168.20.6 -InterfaceIndex $adapter -DefaultGateway 192.168.20.1 -AddressFamily IPv4 -PrefixLength 24; #Setting the IP adress of the server to the one specified at -IPAddress and Gateway specified at -DefaultGateway
 Set-DnsClientServerAddress -InterfaceIndex $adapter -ServerAddresses 192.168.20.6
 Rename-Computer -NewName "IT-Prods-DCServ" ;# Renames the server
-Install-WindowsFeature -Name Feature[DHCP, AD-Domain-Services, DNS] -IncludeManagementTools; # Installs the DHCP windows feature
+Install-WindowsFeature DHCP -IncludeManagementTools; # Installs the DHCP windows feature
 #Install-WindowsFeature AD-Domain-Services -IncludeManagementTools; # Installs the ADDS windows feature
 #Install-WindowsFeature DNS -IncludeManagementTools 
 Restart-Computer
