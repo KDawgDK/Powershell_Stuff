abe3fb278950d4e4750faf3d037bd7eebae76ce4
KDawgDK <48488232+KDawgDK@users.noreply.github.com>
2025-08-18 14:30:48 +0200

    Update Windows_Server_Auto-Setup.ps1
    Emptied the value for the scriptPath variable

--- Windows Server Auto Setup Scripts/Windows_Server_Auto-Setup.ps1
+++ Windows Server Auto Setup Scripts/Windows_Server_Auto-Setup.ps1
@@ -1,6 +1,6 @@
 ## Variables for the configuration
     # Define the path to the PowerShell script
-        $global:scriptPath = "e:\Windows_Server_Auto-Setup.ps1"
+        $global:scriptPath = ""
     # Computer/Server Settings
         $global:adapter = (Get-NetAdapter -Physical | Select-Object -First 1).ifIndex # Do not touch this, it will automatically get the first network adapter
         $global:ComputerName = "" # Change this to whatever you wish it to be
@@ -541,6 +541,7 @@ switch ($Progress) { # Looks for the value and runs the result in the switch sta
     }
 
 }
+
 
 
 
